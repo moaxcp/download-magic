16afc55271150d7709715cb96fbe06a053dfe281
moaxcp@gmail.com <moaxcp@gmail.com@30492dbb-4eab-2d41-bda7-f907a75c390f>
2012-04-30 10:48:15 +0000

    moving directorys

---
+++ Parser/src/penny/parser/WordExtractor.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package penny.parser;
+
+import java.text.StringCharacterIterator;
+
+/**
+ *
+ * @author john
+ */
+public class WordExtractor {
+
+    private WordEater eater;
+    StringBuilder word = new StringBuilder();
+
+    public WordExtractor(WordEater eater) {
+        this.eater = eater;
+    }
+
+    public void put(char c) {
+        if (Character.isLetter(c)) {
+            word.append(c);
+        } else {
+            if(word.length() > 0) {
+                eater.eatWord(word.toString());
+                word.setLength(0);
+            }
+        }
+    }
+
+    public void put(byte[] b, int off, int len) {
+        for (int i = off; i < off + len; i++) {
+            put((char) b[i]);
+        }
+    }
+
+    public String getWordBuffer() {
+        return word.toString();
+    }
+
+    public void setWordBuffer(String wordBuffer) {
+        word.setLength(0);
+        word.append(wordBuffer);
+    }
+}
---
+++ Parser/src/penny/parser/WordReplacer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package penny.parser;
+
+/**
+ *
+ * @author john
+ */
+public interface WordReplacer {
+    public String getReplaceWord(String word);
+}
