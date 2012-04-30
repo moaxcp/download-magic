3b190ac60802045ab2324b9407b3651590a0bb12
moaxcp@gmail.com <moaxcp@gmail.com@30492dbb-4eab-2d41-bda7-f907a75c390f>
2012-04-30 10:43:02 +0000

    Import Parser into SVN

---
+++ Parser/src/com/johnmercier/parser/LinkReplacer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.johnmercier.parser;
+
+/**
+ *
+ * @author moaxcp
+ */
+public interface LinkReplacer {
+    public String getReplaceLink(String link, boolean src);
+}
